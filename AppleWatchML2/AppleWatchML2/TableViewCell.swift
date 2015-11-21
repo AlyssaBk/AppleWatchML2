b98c4425b0698c03edddd3058a9527d196af2198
Peyrot Michael <michael.peyrot@yahoo.fr>
2015-11-21 11:53:29 -0500

    Implementation of numberOfSectionsInTableView, numberOfSections, cellForRowAtIndexPath

--- AppleWatchML2/AppleWatchML2/TableViewCell.swift
+++ AppleWatchML2/AppleWatchML2/TableViewCell.swift
@@ -14,4 +14,6 @@ class TableViewCell: UITableViewCell {
     
     
     
+    
+    
 }
--- AppleWatchML2/AppleWatchML2/ViewController.swift
+++ AppleWatchML2/AppleWatchML2/ViewController.swift
@@ -12,14 +12,26 @@ class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
     }
 
-
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("mainCell", forIndexPath: indexPath)
+        cell.textLabel?.text = "test"
+        
+        return cell
+    }
+    
 }
 
