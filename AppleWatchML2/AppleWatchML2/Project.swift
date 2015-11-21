2f6772ea83f6fd84680dd543c2f0b4be93901ba6
Samuel Bellerose <samuel.bellerose@icloud.com>
2015-11-21 11:50:46 -0500

    Added Project model

---
+++ AppleWatchML2/AppleWatchML2/Project.swift
@@ -0,0 +1,13 @@
+//
+//  Project.swift
+//  AppleWatchML2
+//
+//  Created by Samuel Bellerose on 2015-11-21.
+//  Copyright © 2015 Samuel Bellerose. All rights reserved.
+//
+
+class Project {
+    var id: Int
+    var name: String
+    var link: String
+}
