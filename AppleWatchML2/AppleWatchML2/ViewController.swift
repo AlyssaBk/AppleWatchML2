003c86923c8fd74c55adf79ee1f50383e4b259d7
Peyrot Michael <michael.peyrot@yahoo.fr>
2015-11-21 17:01:05 -0500

    Added Edit/Cancel button

--- AppleWatchML2/AppleWatchML2/ViewController.swift
+++ AppleWatchML2/AppleWatchML2/ViewController.swift
@@ -13,6 +13,20 @@ import Alamofire
 class ViewController: UITableViewController {
     
     var requestUtilityObject = RequestUtility()
+    var editButtonSelected = false
+
+    @IBAction func editButtonAction(sender: UIButton, forEvent event: UIEvent) {
+        if(editButtonSelected == false){
+            tableView.setEditing(true, animated: true)
+            sender.setTitle("Cancel", forState: .Normal)
+            editButtonSelected = true
+        }
+        else if(editButtonSelected == true){
+            tableView.setEditing(false, animated: true)
+            sender.setTitle("Edit", forState: .Normal)
+            editButtonSelected = false
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,8 +57,23 @@ class ViewController: UITableViewController {
         let cell = tableView.dequeueReusableCellWithIdentifier("mainCell", forIndexPath: indexPath)
         let project = requestUtilityObject.getProjectsList()[indexPath.row]
         cell.textLabel?.text = project.name
+        cell.textLabel?.font = UIFont(name:"Avenir", size:11)
         return cell
     }
     
+    override func tableView(tableView: UITableView, editingStyleForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCellEditingStyle {
+        return .None
+    }
+    
+    override func tableView(tableView: UITableView, shouldIndentWhileEditingRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return false
+    }
+    
+    override func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) {
+        var project = requestUtilityObject.getProjectsList()
+        let movedObject = project[sourceIndexPath.row]
+        project.removeAtIndex(sourceIndexPath.row)
+        project.insert(movedObject, atIndex: destinationIndexPath.row)
+    }
 }
 
