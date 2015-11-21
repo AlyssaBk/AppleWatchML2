f1ab4885c7e6cb336222548bca674bb1eeba69be
Samuel Bellerose <samuel.bellerose@icloud.com>
2015-11-21 10:17:14 -0500

    Premier commit

---
+++ AppleWatchML2/AppleWatchML2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppleWatchML2
+//
+//  Created by Samuel Bellerose on 2015-11-21.
+//  Copyright © 2015 Samuel Bellerose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
