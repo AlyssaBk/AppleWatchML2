40b6d447403f3e2c0ba3e5e67b4d80ffbc7abfae
Peyrot Michael <michael.peyrot@yahoo.fr>
2015-11-21 10:59:56 -0500

    Added TableviewCell

---
+++ AppleWatchML2/AppleWatchML2/TableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TableViewCell.swift
+//  AppleWatchML2
+//
+//  Created by Peyrot Michael on 2015-11-21.
+//  Copyright © 2015 Samuel Bellerose. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var projectNameLabel: UILabel!
+    
+    
+    
+}
